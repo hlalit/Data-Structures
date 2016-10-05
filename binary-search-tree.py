f9ada4f6ac1313cda9e96d15277bbd8d19cd1225
Harshad Lalit <harshad.lalit@gmail.com>
2016-10-05 00:02:07 -0400

    Add files via upload

--- binary-search-tree.py
+++ binary-search-tree.py
@@ -73,9 +73,97 @@ class binary_search_tree:
 
         print(node.value)
 
+
+    def search(self, node, parent, item):
+
+        if(node.value == item):         #node to delete does not have any children
+            if(node.left == None and node.right == None):
+                self.node = node
+                self.parent = parent
+                self.flag = 1
+
+            elif(node.left == None and node.right != None):     #node to delete does not have a left child but a right child
+                self.node = node
+                self.parent = parent
+                self.flag = 2
+
+            elif(node.left != None and node.right == None):     #node to delete does not have a right child but a left child
+                self.node = node
+                self.parent = parent
+                self.flag = 3
+
+            elif(node.left != None and node.right != None):     #node to delete has both the left and right child
+                self.node = node
+                self.parent = parent
+                self.flag = 4
+        else:
+            if node.left:
+                self.search(node.left, node, item)
+
+            if node.right:
+                self.search(node.right, node, item)
+
+            return
+
+    def getMin(self, root):
+
+        self.parent_of_min = self.node
+        while 1:
+            if root.left:
+                self.parent_of_min = root
+                root = root.left
+                continue
+            else:
+                self.min = root
+                break
+
     def remove(self, node):
 
-        
+        print("Enter element you want to remove: ")
+        element = input()
+        element = int(element)
+
+        self.search(node, None, element)
+
+        if (self.flag == 1):    #node to delete does not have any children
+            if(self.node.value == self.parent.left.value):
+                self.parent.left = None
+            else:
+                self.parent.right = None
+
+        if(self.flag == 2):     #node to delete does not have a left child but a right child
+            if(self.node.value == self.parent.left.value):
+                self.parent.left = self.node.right
+            else:
+                self.parent.right = self.node.right
+
+        if (self.flag == 3):    #node to delete does not have a right child but a left child
+            if (self.node.value == self.parent.left.value):
+                self.parent.left = self.node.left
+            else:
+                self.parent.right = self.node.left
+
+        if(self.flag == 4):     #node to delete has both the left and right child
+            self.getMin(self.node.right)        #finding minimum of the right subtree
+
+            if(self.min.right == None):                             #if the minimum node has no right child
+                if (self.node.value != self.parent_of_min.value):
+                    self.parent_of_min.left = None
+                    self.node.value = self.min.value
+
+                if (self.node.value == self.parent_of_min.value):
+                    self.node.right = None
+                    self.node.value = self.min.value
+
+            if(self.min.right != None):                               #if the minimum node has a right child still
+                if(self.node.value != self.parent_of_min.value):
+                    self.parent_of_min.left = self.min.right
+                    self.node.value = self.min.value
+
+                if(self.node.value == self.parent_of_min.value):
+                    self.node.right = self.min.right
+                    self.node.value = self.min.value
+
         return
 
 tree = binary_search_tree() #tree is object of the binary_search_tree class
